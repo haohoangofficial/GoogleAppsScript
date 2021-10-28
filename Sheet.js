ab03d83d2ba48db241a52df4737d060aa1a32c8d
Hao Hoang <38025803+haohoangofficial@users.noreply.github.com>
2021-10-28 20:27:05 +0700

    update some basic functions

--- Sheet.js
+++ Sheet.js
@@ -6,6 +6,15 @@
 const Sheet = {
     /**
      * 
+     * @param {*} sheetID Sheet ID value
+     * @param {*} sheetName Sheet Name 
+     * @returns 
+     */
+    getSheetName: function(sheetID, sheetName) {
+        return SpreadsheetApp.openById(sheetID).getSheetByName(sheetName)
+    },
+    /**
+     * 
      * @param {*} num Number
      * @returns Letter
      * Example 0 = A; 1 = B
@@ -17,5 +26,29 @@ const Sheet = {
             num = Math.floor(num / 26) - 1
         }
         return letters
+    },
+    /**
+     * 
+     * @param {*} table Data returned after the method getSheetName
+     * @returns Number of last row
+     */
+    countLastRow: function(table) {
+        return table.getDataRange().getLastRow()
+    },
+    /**
+     * 
+     * @param {*} table Data returned after the method getSheetName
+     * @returns Number of last column
+     */
+    countLastColumn: function(table) {
+        return table.getDataRange().getLastColumn()
+    },
+    /**
+     * 
+     * @param {*} table Data returned after the method getSheetName
+     * @returns return first row data
+     */
+    getHeader: function(table) {
+        return (table.getRange(`A1:${colName(countLastColumn(table) - 1)}1`).getValues())[0]
     }
 }
